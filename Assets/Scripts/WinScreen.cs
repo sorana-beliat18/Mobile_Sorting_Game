23c29ceb754789a3b5ec6f1dec51b6913f9beb07
sorana-beliat18 <sorana.beliat8@gmail.com>
2026-06-12 11:56:31 +0300

    Win Screen character floating

--- Assets/Scripts/WinScreen.cs
+++ Assets/Scripts/WinScreen.cs
@@ -17,9 +17,9 @@ public class WinScreen : MonoBehaviour
     }
 
     public CharacterData[] allCharacters;
-    public float bounceSpeed = 6f;     
-    public float bounceHeight = 30f;     
-    public float squishAmount = 0.15f;   
+    public float floatSpeed = 2f;     
+    public float floatAmount = 10f;     
+  
 
     private Image characterImage;
     private Vector2 startPos;
@@ -57,10 +57,7 @@ public class WinScreen : MonoBehaviour
     void Update()
     {
         animationTimer += Time.deltaTime;
-        float newY = startPos.y + Mathf.Abs(Mathf.Sin(animationTimer * bounceSpeed)) * bounceHeight;
+        float newY = startPos.y + Mathf.Abs(Mathf.Sin(animationTimer * floatSpeed)) *floatAmount;
         characterImage.rectTransform.anchoredPosition = new Vector2(startPos.x, newY);
-        float scaleX = startScale.x + Mathf.Cos(animationTimer * bounceSpeed * 2f) * squishAmount;
-        float scaleY = startScale.y - Mathf.Cos(animationTimer * bounceSpeed * 2f) * squishAmount;
-        transform.localScale = new Vector3(scaleX, scaleY, 1f);
     }
 }
